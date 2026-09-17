3307272f32f108d4ec217d70a83c4182d1fcfa8a
DENIMAL Simon <s.denimal@rubika-edu.com>
2026-09-17 17:24:35 +0200

    [MODIFIED] vanishing Banners

--- BuglessBugGame/Assets/Scripts/UI/UIJourney.cs
+++ BuglessBugGame/Assets/Scripts/UI/UIJourney.cs
@@ -18,9 +18,13 @@ public class UIJourney : MonoBehaviour
     [SerializeField] private GameObject bugSumPrefab;
     [SerializeField] private GameObject bugSumBannerPrefab;
     
+    [Header("Banner Settings")]
+    [SerializeField] private float bannerLifetime = 3f;
+    
     private List<InsectSlot> _insects = new List<InsectSlot>();
     private List<GameObject> _bugBanners = new List<GameObject>();
-    
+    private Dictionary<GameObject, Coroutine> _bannerCoroutines = new Dictionary<GameObject, Coroutine>();
+
     private void OnEnable()
     {
         PlayerInsectPickUp.OnInsectPicked += SpawnInsectSumBanner;
@@ -54,45 +58,83 @@ public class UIJourney : MonoBehaviour
                 slotUI.Setup(slot.insect, slot.count);
         }
     }
+
     private void ReturnLobby()
     {
+        _parentJourneySum.gameObject.SetActive(false);
         GameManager.instance.player.playerData.playerInventoryData.insects.Clear();
         SceneManager.LoadScene(GameManager.instance._lobbySceneName);
     }
 
     private void SpawnInsectSumBanner(InsectData insect, int count)
     {
-        int maxBannersVisibles = 3;
-        
-        
-        
-        foreach (Transform child in _bugBannerSpawn)
+        int existingIndex = _insects.FindIndex(s => s.insect == insect);
+
+        if (existingIndex != -1)
         {
-            Destroy(child.gameObject);
+            InsectSlot slot = _insects[existingIndex];
+            slot.count += count;
+            _insects[existingIndex] = slot;
+
+            GameObject existingBanner = _bugBanners[existingIndex];
+            BugSlotUI slotUI = existingBanner.GetComponent<BugSlotUI>();
+            if (slotUI != null)
+                slotUI.Setup(slot.insect, slot.count);
+            if (_bannerCoroutines.TryGetValue(existingBanner, out Coroutine activeCoroutine))
+            {
+                if (activeCoroutine != null) StopCoroutine(activeCoroutine);
+            }
+            _bannerCoroutines[existingBanner] = StartCoroutine(RemoveBannerAfterDelay(existingBanner, bannerLifetime));
         }
-        
-        foreach (InsectSlot slot in GameManager.instance.player.playerData.playerInventoryData.insects)
+        else
         {
-            if (slot.insect == null) continue;
-            
-            if (_insects.Count >= maxBannersVisibles)
+            int maxBannersVisibles = 3;
+            if (_bugBanners.Count >= maxBannersVisibles)
             {
                 GameObject oldBanner = _bugBanners[0];
                 _bugBanners.RemoveAt(0);
-                _insects.RemoveAt(0);
-                Destroy(oldBanner);
-            }
-            
-            GameObject row = Instantiate(bugSumBannerPrefab, _bugBannerSpawn);
-            
-            _bugBanners.Add(row);
-            
-            BugSlotUI slotUI = row.GetComponent<BugSlotUI>();
+                if (_insects.Count > 0) _insects.RemoveAt(0);
 
+                if (oldBanner != null)
+                {
+                    if (_bannerCoroutines.TryGetValue(oldBanner, out Coroutine oldCoroutine))
+                    {
+                        if (oldCoroutine != null) StopCoroutine(oldCoroutine);
+                        _bannerCoroutines.Remove(oldBanner);
+                    }
+                    Destroy(oldBanner);
+                }
+            }
+
+            GameObject row = Instantiate(bugSumBannerPrefab, _bugBannerSpawn);
+            _bugBanners.Add(row);
+
+            BugSlotUI slotUI = row.GetComponent<BugSlotUI>();
             if (slotUI != null)
-                slotUI.Setup(slot.insect, slot.count);
-            
-            _insects.Add(slot);
+                slotUI.Setup(insect, count);
+
+            InsectSlot slotData = new InsectSlot { insect = insect, count = count };
+            _insects.Add(slotData);
+
+            _bannerCoroutines[row] = StartCoroutine(RemoveBannerAfterDelay(row, bannerLifetime));
         }
+    }
+
+    private IEnumerator RemoveBannerAfterDelay(GameObject banner, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (banner == null) yield break;
+
+        int index = _bugBanners.IndexOf(banner);
+        if (index != -1)
+        {
+            _bugBanners.RemoveAt(index);
+            if (index < _insects.Count) _insects.RemoveAt(index);
+        }
+
+        if (_bannerCoroutines.ContainsKey(banner)) _bannerCoroutines.Remove(banner);
+
+        Destroy(banner);
     }
 }
