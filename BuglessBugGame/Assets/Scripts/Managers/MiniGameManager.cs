10ecb38031ce7b5c6749b133e5510464ad2f3012
DENIMAL Simon <s.denimal@rubika-edu.com>
2026-09-19 15:54:47 +0200

    [MODIFIED] Bug show Minigame

--- BuglessBugGame/Assets/Scripts/Managers/MiniGameManager.cs
+++ BuglessBugGame/Assets/Scripts/Managers/MiniGameManager.cs
@@ -1,11 +1,64 @@
+using System.Collections.Generic;
+using GamePlayCore;
 using UnityEngine;
 
 public class MiniGameManager : MonoBehaviour
 {
-    private MiniGameUI M_miniGameUI;
+    [SerializeField] private GameObject instanceMinigameParent;
 
-    private void DetectInteraction()
+    private GameObject currentMiniGameInstance;
+    private MiniGameUI currentMiniGameUI;
+    private Insect currentInsect;
+
+    public bool IsMiniGameActive => currentMiniGameInstance != null;
+
+    public void DetectForInteraction(Insect insect)
     {
-        
+        if (IsMiniGameActive) return;
+
+        List<InsectSlot> insectList = Player.Instance.playerData.playerInventoryData.insects;
+        InsectSlot existingSlot = insectList.Find(slot => slot.insect == insect.bug);
+        if (existingSlot != null && existingSlot.count == insect.bug.maxStackable) return;
+
+        if (insect.bug.catchMiniGamePrefabUI == null)
+        {
+            Debug.LogWarning($"No catchMiniGamePrefabUI set on {insect.bug.name}");
+            return;
+        }
+
+        currentInsect = insect;
+        currentMiniGameInstance = Instantiate(insect.bug.catchMiniGamePrefabUI, instanceMinigameParent.transform);
+        currentMiniGameUI = currentMiniGameInstance.GetComponent<MiniGameUI>();
+    }
+
+    public void OnMiniGameSuccess()
+    {
+        if (currentInsect == null) return;
+
+        Player.Instance.insectPickUp.AddItem(currentInsect.bug, currentInsect.count);
+        Destroy(currentInsect.gameObject);
+
+        Player player = GameManager.instance.player;
+        player.isInteracted = false;
+
+        Debug.Log($"{currentInsect.bug.name} has been picked up");
+
+        CloseMiniGame();
+    }
+
+    public void OnMiniGameFailed()
+    {
+        CloseMiniGame();
+    }
+
+    private void CloseMiniGame()
+    {
+        if (currentMiniGameInstance != null)
+        {
+            Destroy(currentMiniGameInstance);
+        }
+        currentMiniGameInstance = null;
+        currentMiniGameUI = null;
+        currentInsect = null;
     }
 }
